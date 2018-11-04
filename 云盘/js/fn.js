722748287a2b9f35e6c8377e6e3aeb5b8e46f5dd
ggapsong <1966588810@qq.com>
2018-11-04 22:10:52 +0800

    事件

--- 云盘/js/fn.js
+++ 云盘/js/fn.js
@@ -1,8 +1,35 @@
 function msg(str,data){
     var text = str;
-    var color = data.color;
-    var icon = data.icon;
-    var iconArr = ['../img/icon1','../img/icon2'];
-    var htmlText = `<div style='background-color:"${color}"'><img src='${iconArr[icon]}'/><span>${text}</span></div>`;
-    console.log(htmlText);
+    var parent = document.querySelector('.msg_box');
+    var iconArr = ['../img/bingo.png','../img/jinggaoExclamation.png'];
+    var div = document.createElement('div');
+    div.className = 'delsuc_box';
+    div.style.backgroundColor= data.color;
+    div.innerHTML = `<img src="${iconArr[data.icon]}" alt="">
+                        <span>${text}</span>`;
+    parent.appendChild(div);
+    
+    mTween({
+        el:div,
+        attr:{
+            top:0
+        },
+        time:20,
+        cb(){
+            setTimeout(()=>{
+                mTween({
+                    el:div,
+                    attr:{
+                        top:-40
+                    },
+                    time:20,
+                    cb(){
+                        setTimeout(()=>{
+                            parent.removeChild(div);
+                        },1000)
+                    }
+                });
+            },1000)
+        }
+    });
 }
